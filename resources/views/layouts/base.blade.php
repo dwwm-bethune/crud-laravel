c98bc52fcf8d8293decf7f1869c460eaf4460d80
Matthieu Mota <matthieumota@gmail.com>
2022-09-15 12:27:12 +0200

    Configure Tailwind

---
+++ resources/views/layouts/base.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>{{ config('app.name') }}</title>
+
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+<body>
+    @yield('content')
+</body>
+</html>
