0d44795dea70e4101fbee65d063adbb8126b2d7e
Matthieu Mota <matthieumota@gmail.com>
2022-09-15 13:21:20 +0200

    Front

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -3,12 +3,40 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>{{ config('app.name') }}</title>
+
+    <!-- Fonts -->
+    <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
 
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
-<body>
+<body class="font-[Nunito] bg-gray-800 text-gray-300">
+    <header>
+        <div class="max-w-6xl mx-auto px-3">
+            <div class="flex justify-between items-center">
+                <h1 class="text-2xl font-bold py-6">
+                    <a href="#">CRUD</a>
+                </h1>
+
+                <ul class="flex space-x-6">
+                    <li>
+                        <a class="border-b hover:border-gray-300 duration-300 pb-2 {{ request()->routeIs('home') ? 'border-gray-300' : 'border-transparent'}}" href="{{ route('home') }}">Accueil</a>
+                    </li>
+                    <li>
+                        <a class="border-b hover:border-gray-300 duration-300 pb-2 {{ request()->routeIs('index') ? 'border-gray-300' : 'border-transparent'}}" href="#">Liste</a>
+                    </li>
+                    <li>
+                        <a class="border-b hover:border-gray-300 duration-300 pb-2 {{ request()->routeIs('create') ? 'border-gray-300' : 'border-transparent'}}" href="#">Créer</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </header>
+
     @yield('content')
+
+    <footer>
+        <p class="text-center">{{ config('app.name') }} - Copyright &copy; {{ now()->year }}.</p>
+    </footer>
 </body>
 </html>
