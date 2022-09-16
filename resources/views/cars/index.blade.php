25b474d9759169685c2008e66f35c80a2073ba51
Matthieu Mota <matthieumota@gmail.com>
2022-09-16 11:18:21 +0200

    Front

--- resources/views/cars/index.blade.php
+++ resources/views/cars/index.blade.php
@@ -3,10 +3,10 @@
 @section('content')
     <div class="flex flex-wrap -mx-3">
         @forelse ($cars as $car)
-            <div class="w-1/3">
-                <div class="mx-5 overflow-hidden">
+            <div class="w-full md:w-1/2 lg:w-1/3">
+                <div class="mx-5 mb-10 overflow-hidden">
                     <a href="{{ route('cars.show', [$car->id, $car->slug]) }}" class="group">
-                        <img class="w-full h-64 object-cover group-hover:scale-110 duration-200" src="{{ asset($car->image) }}" alt="{{ $car->name }}">
+                        <img class="rounded shadow-xl w-full h-64 object-cover group-hover:scale-110 duration-200" src="{{ asset($car->image) }}" alt="{{ $car->name }}">
                         <div>
                             <h3 class="text-center text-xl font-semibold mt-4">{{ $car->name }}</h3>
                             <p class="text-center text-xs">{{ $car->created_at->translatedFormat('l d F Y') }} - {{ $car->created_at->ago() }}</p>
--- resources/views/cars/show.blade.php
+++ resources/views/cars/show.blade.php
@@ -1,16 +1,16 @@
 @extends('layouts.base')
 
 @section('content')
-    <div class="flex justify-between space-x-6">
-        <div class="w-1/2">
-            <h1 class="text-center text-3xl font-semibold mt-4">{{ $car->name }}</h1>
+    <div class="flex flex-wrap lg:flex-nowrap lg:justify-between lg:space-x-6">
+        <div class="order-1 lg:order-none lg:w-1/2 w-full">
+            <h1 class="text-center text-5xl font-bold">{{ $car->name }}</h1>
             <p class="text-center text-xs">{{ $car->created_at->translatedFormat('l d F Y') }} - {{ $car->created_at->ago() }}</p>
-            <div class="text-justify mt-5">
+            <div class="text-justify mt-5 prose max-w-none prose-invert">
                 {!! str($car->content)->markdown() !!}
             </div>
         </div>
-        <div class="w-1/2">
-            <img class="w-full object-cover group-hover:scale-110 duration-200" src="{{ asset($car->image) }}" alt="{{ $car->name }}">
+        <div class="lg:w-1/2">
+            <img class="mb-10 lg:mb-0 rounded shadow-xl w-full object-cover group-hover:scale-110 duration-200" src="{{ asset($car->image) }}" alt="{{ $car->name }}">
         </div>
     </div>
 @endsection
